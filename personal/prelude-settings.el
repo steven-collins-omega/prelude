4e5a649bc791869c92707a9fad02dec8cf03102e
jilen <jilen.zhang@gmail.com>
2014-01-01 20:45:40 +0800

    Revert "change default theme"
    This reverts commit e64569393e57a55c558b5a547427899b7ed07cc7.

--- personal/prelude-settings.el
+++ personal/prelude-settings.el
@@ -2,11 +2,8 @@
 
 ;;; Commentary:
 ;;
-;;; Code:
 
-(prelude-require-package 'solarized-theme)
-(disable-theme 'zenburn)
-(load-theme 'solarized-dark t)
+
 (provide 'prelude-settings)
 
 ;;; prelude-settings.el ends here
